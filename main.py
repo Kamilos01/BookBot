5a6765d84d6dff8d20db8893e088b010ebbef9b6
Kamil Byrski <kamil.byrski1997@gmail.com>
2024-08-03 21:38:03 +0200

    Added count_characters and convert_to_lower functions in main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    lower_text = convert_to_lower(text)
+    dict_counted_characters = count_characters(lower_text)
+    print(dict_counted_characters)
 
 def get_num_words(text):
     words = text.split()
@@ -11,5 +13,19 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def convert_to_lower(text):
+    lowered_text = text.lower()
+    return lowered_text
+
+def count_characters(lower_text):
+    char_dict = {}
+    for char in lower_text:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] +=1
+    return char_dict
+
 
 main()
