7b5a289e1bc858f27f491163ed25205c74c7d37e
Kamil Byrski <kamil.byrski1997@gmail.com>
2024-08-03 22:28:53 +0200

    Added sort_on function in main.py

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ def main():
     num_words = get_num_words(text)
     lower_text = convert_to_lower(text)
     dict_counted_characters = count_characters(lower_text)
-    print(dict_counted_characters)
+    chars_sorted_list = sort_on(dict_counted_characters)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['value']} times")
+
+    print(f"--- End report ---")
+
 
 def get_num_words(text):
     words = text.split()
@@ -26,6 +37,16 @@ def count_characters(lower_text):
         else:
             char_dict[char] +=1
     return char_dict
+
+def get_value(item):
+    return item['value']
+
+def sort_on(dict):
+    list_of_dicts = []
+    for char in dict:
+        list_of_dicts.append({"char": char, "value": dict[char]})
+    list_of_dicts.sort(key=get_value, reverse=True)
+    return list_of_dicts
 
 
 main()
